63b39663f36e2705507d5f74d5ccec136c747e04
Natalia Gubanova <42213097+khomtali@users.noreply.github.com>
2019-04-10 19:25:30 +0200

    Add solution
    from My CodePen
    https://codepen.io/khomtali/pen/BMeNOx

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,12 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  const header = event.target;
+  if (header.hasAttribute('data-dir')) {
+    header.dataset.dir = -1 * +(header.dataset.dir);
+  } else {
+    header.setAttribute('data-dir', 1);
+  }
+  event.currentTarget.setAttribute('data-sort-by', header.dataset.propName);
+  sortTable(event.currentTarget.dataset.sortBy, header.dataset.dir);
 }
