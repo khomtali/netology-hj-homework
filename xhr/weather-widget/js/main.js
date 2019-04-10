7952c9a22e22d0fc04542babff2df7090833e1bc
Natalia Gubanova <42213097+khomtali@users.noreply.github.com>
2019-04-10 18:35:30 +0200

    Add solution
    from my CodePen
    https://codepen.io/khomtali/pen/pGKWJL

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,5 +1,5 @@
 const request = new XMLHttpRequest();
-request.open('GET', 'https://neto-api.herokuapp.com/weather', false);
+request.open('GET', 'https://neto-api.herokuapp.com/weather', true);
 request.send();
 if (request.status === 200) {
   const response = JSON.parse(request.responseText);
