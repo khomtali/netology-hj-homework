26da32020765c0770907b3da1ece5d8b37890e63
Natalia Gubanova <42213097+khomtali@users.noreply.github.com>
2019-04-10 21:12:39 +0200

    Add solution
    from my CodePen
    https://codepen.io/khomtali/pen/vPBgey

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const counter = document.querySelector('.counter');
+const errors = document.querySelector('.errors');
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+window.addEventListener('beforeunload', () => {
+  connection.close(1000);
+});
+connection.addEventListener('message', event => {
+  let info = JSON.parse(event.data);
+  counter.textContent = info.connections;
+  errors.textContent = info.errors;
+});
