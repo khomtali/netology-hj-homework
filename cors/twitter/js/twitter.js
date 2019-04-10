66cd0bade465a469ce3830ce2fa1bc88e1f05e4a
Natalia Gubanova <42213097+khomtali@users.noreply.github.com>
2019-04-10 21:19:28 +0200

    Add solution
    from my CodePen
    https://codepen.io/khomtali/pen/ywewjJ

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// функция подстановки полученных данных профиля в HTML-разметку
+function makeUserVidget(userData) {
+  document.querySelector('[data-wallpaper]').src = userData.wallpaper;
+  document.querySelector('[data-username]').textContent = userData.username;
+  document.querySelector('[data-description]').textContent = userData.description;
+  document.querySelector('[data-pic]').src = userData.pic;
+  document.querySelector('[data-tweets]').textContent = userData.tweets;
+  document.querySelector('[data-followers]').textContent = userData.followers;
+  document.querySelector('[data-following]').textContent = userData.following;
+}
+
+// реализация fetch-запроса данных профиля
+function loadData(url) {
+  return new Promise((done, fail) => {
+    window.getUserData = done;
+    const script = document.createElement('script');
+    script.src = `${url}?callback=getUserData`;
+    document.querySelector('.container').appendChild(script);
+  });
+}
+
+// запрос данных и последующий вызов функции makeUserVidget
+// с полученными данными в качестве аргумента
+loadData('https://neto-api.herokuapp.com/twitter/jsonp')
+  .then(makeUserVidget);
