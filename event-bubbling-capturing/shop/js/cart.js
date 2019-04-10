a0b901405a6e0c238fb97144aef7532ad3fb81f4
Natalia Gubanova <42213097+khomtali@users.noreply.github.com>
2019-04-10 20:59:00 +0200

    Add solution
    from my CodePen
    https://codepen.io/khomtali/pen/qgGLqy

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,24 @@
 'use strict';
+
+let cartButtons = document.querySelectorAll('.add-to-cart');
+const addItems = document.querySelector('.show-more');
+addItems.addEventListener('click', updateBtns);
+
+for (let cartBtn of cartButtons) {
+  cartBtn.addEventListener('click', handleAddToCart);
+}
+
+function handleAddToCart(event) {
+  event.preventDefault();
+  const item = {};
+  item.title = event.target.dataset.title;
+  item.price = event.target.dataset.price;
+  addToCart(item);
+}
+
+function updateBtns() {
+  cartButtons = document.querySelectorAll('.add-to-cart');
+  for (let cartBtn of cartButtons) {
+    cartBtn.addEventListener('click', handleAddToCart);
+  }
+}
