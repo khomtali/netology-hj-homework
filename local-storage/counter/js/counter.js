421d0fa8d5d7b6b25f00a421bc95f6a8aae1a4e6
Natalia Gubanova <42213097+khomtali@users.noreply.github.com>
2019-04-10 21:01:46 +0200

    Add solution
    from my CodePen
    https://codepen.io/khomtali/pen/ErBGBR

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const counter = window.counter;
+document.querySelector('.wrap-btns').addEventListener('click', changeValue);
+if (localStorage.value) {
+  counter.innerHTML = localStorage.value;
+} else {
+  localStorage.value = 0;
+  counter.innerHTML = 0;
+}
+
+function changeValue(event) {
+  if (event.target == window.increment) {
+    localStorage.value++;
+  } else if (event.target == window.decrement && localStorage.value > 0) {
+    localStorage.value--;
+  } else if (event.target == window.reset) {
+    localStorage.value = 0;
+  }
+  counter.innerHTML = localStorage.value;
+}
